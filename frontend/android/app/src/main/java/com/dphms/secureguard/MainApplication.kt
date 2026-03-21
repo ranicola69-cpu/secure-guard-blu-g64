912fd67a4905f448107ba8cea5a6e17c7e8f04d4
ranicola69-cpu <ranicola69@gmail.com>
2026-03-21 00:13:48 -0400

    fix: RN 0.79 MainApplication - remove ReactNativeApplicationEntryPoint, add reactNativeHost stub, use SoLoader.init + DefaultNewArchitectureEntryPoint.load()

---
+++ frontend/android/app/src/main/java/com/dphms/secureguard/MainApplication.kt
@@ -0,0 +1,52 @@
+package com.dphms.secureguard
+
+import android.app.Application
+import android.content.res.Configuration
+
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactHost
+import com.facebook.react.common.ReleaseLevel
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.soloader.SoLoader
+
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ExpoReactHostFactory
+
+class MainApplication : Application(), ReactApplication {
+
+  override val reactNativeHost: ReactNativeHost
+    get() = throw UnsupportedOperationException(
+      "New Architecture (bridgeless) is enabled. Use reactHost instead of reactNativeHost."
+    )
+
+  override val reactHost: ReactHost by lazy {
+    ExpoReactHostFactory.getDefaultReactHost(
+      context = applicationContext,
+      packageList =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        }
+    )
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    DefaultNewArchitectureEntryPoint.releaseLevel = try {
+      ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
+    } catch (e: IllegalArgumentException) {
+      ReleaseLevel.STABLE
+    }
+    DefaultNewArchitectureEntryPoint.load()
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+  }
+}
